3f8520c4c07344c8058a87049c39431a9fcc5d49
NathanielCaron <nathaniel.caron22@gmail.com>
2020-10-06 10:59:55 -0300

    As 4: Continued work, cleaned up code

--- as4/nathaniel_caron_assignment4.c
+++ as4/nathaniel_caron_assignment4.c
@@ -18,43 +18,63 @@ int randomTime();
 int time_limit = 0;
 time_t start_time = 0;
 time_t current_time = 0;
-
 int paddles = 0;
 int canoes = 0;
-
 static pthread_mutex_t paddle_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t canoe_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+// Function to be executed by the paddle thread
 void *paddle_function(void *arg) {
-    int random_time = 0;
+    int random_wait_time = 0;
     current_time = time(NULL);
+
     while ((current_time - start_time) < time_limit) {
-        if (paddles > 2) {
-            random_time = randomTime();
-            sleep(random_time);
+        if (paddles < 2) {
+            random_wait_time = randomTime();
+            sleep(random_wait_time);
             pthread_mutex_lock(&paddle_mutex);
                 paddles++;
+                printf("We have a paddle.\n");
             pthread_mutex_unlock(&paddle_mutex);
-            printf("We have a paddle.\n");
         }
-
-        printf("paddles function with time %d\n", random_time);
         current_time = time(NULL);
     }
 }
 
+// Function to be executed by the canoe thread
 void *canoe_function(void *arg) {
+    int random_wait_time = 0;
+    current_time = time(NULL);
 
-    pthread_mutex_lock(&canoe_mutex);
-    pthread_mutex_unlock(&canoe_mutex);
+    while ((current_time - start_time) < time_limit) {
+        if (canoes < 1) {
+            random_wait_time = randomTime();
+            sleep(random_wait_time);
+            pthread_mutex_lock(&canoe_mutex);
+                canoes++;
+                printf("We have a canoe.\n");
+            pthread_mutex_unlock(&canoe_mutex);
+        }
+        current_time = time(NULL);
+    }
 }
 
+// Function to be executed by the shipper thread
 void *shipper_function(void *arg) {
-    
-    pthread_mutex_lock(&paddle_mutex);
-    pthread_mutex_unlock(&paddle_mutex);
-    pthread_mutex_lock(&canoe_mutex);
-    pthread_mutex_unlock(&canoe_mutex);
+    current_time = time(NULL);
+
+    while ((current_time - start_time) <= time_limit || (paddles == 2 && canoes == 1)) {
+        if (paddles == 2 && canoes == 1) {
+            pthread_mutex_lock(&paddle_mutex);
+                pthread_mutex_lock(&canoe_mutex);
+                    paddles -= 2;
+                    canoes--;
+                    printf("We now have a shipment!\n");
+                pthread_mutex_unlock(&canoe_mutex);
+            pthread_mutex_unlock(&paddle_mutex);
+        }
+        current_time = time(NULL);
+    }
 }
 
 int main(int argc, char **argv) {
@@ -70,21 +90,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    printf("time limit: %d\n", time_limit);
-
-    sleep(1);
-
-    current_time = time(NULL);
-
-    printf("time difference: %ld\n", current_time - start_time);
-
-    printf("Random time: %d s\n", randomTime());
-    printf("Random time: %d s\n", randomTime());
-    printf("Random time: %d s\n", randomTime());
-    printf("Random time: %d s\n", randomTime());
-    printf("Random time: %d s\n", randomTime());
-
-    // Create and join all threads
+    // Create all threads
     pthread_t thread_paddle;
     pthread_t thread_canoe;
     pthread_t thread_shipper;
@@ -93,6 +99,7 @@ int main(int argc, char **argv) {
     pthread_create(&thread_canoe, NULL, &canoe_function, NULL);
     pthread_create(&thread_shipper, NULL, &shipper_function, NULL);
 
+    // Join all threads
     pthread_join(thread_paddle, NULL);
     pthread_join(thread_canoe, NULL);
     pthread_join(thread_shipper, NULL);
