6ed3c96fc7c5ddf2defb6c57d31e4f360689b87a
nathanielcaron <nathaniel.caron22@gmail.com>
2020-10-15 22:57:41 -0300

    As 5: Completed work

--- as5/nathaniel_caron_assignment5.c
+++ as5/nathaniel_caron_assignment5.c
@@ -13,13 +13,14 @@
  * Solution:
  * With the use of semaphores this program allows all cars to cross the bridge.
  * To avoid STARVATION:
- * If 5 cars in a row in the same direction advance on bridge, then cars in the other direction will be allowed to advance.
+ * If 5 cars in a row in the same direction advance on bridge and there are cars in the other direction,
+ * then cars in the other direction will be allowed to advance.
  */
 
 // Structure to represent a car thread
 typedef struct Car {
     char *car;
-    char *direction;
+    char direction;
     int arrival;
     int duration;
     int duration_left;
@@ -34,9 +35,9 @@ sem_t bridge_sem;
 Car *cars;
 int car_count = 0;
 int cars_on_bridge = 0;
-int cars_since_direction_change = 1;
-char *current_direction;
-char *next_direction;
+int cars_since_direction_change = 0;
+char current_direction;
+char next_direction;
 int earliest_arrival = 0;
 int current_time = 0;
 int thread_num = 0;
@@ -49,8 +50,6 @@ int getProcesses(Car **cars, size_t *cars_size);
 
 int main(int argc, char **argv) {
     int i = 0;
-    current_direction = (char*)calloc(4, sizeof(current_direction));
-    next_direction = (char*)calloc(4, sizeof(next_direction));
 
     // Initialize cars, VARIABLE SIZE
     size_t initial_size;
@@ -65,13 +64,13 @@ int main(int argc, char **argv) {
 
     // Determine the earliest arrival time and determine initial direction
     earliest_arrival = cars[0].arrival;
-    strcpy(current_direction, cars[0].direction);
-    strcpy(next_direction, cars[0].direction);
+    current_direction = cars[0].direction;
+    next_direction = cars[0].direction;
     for (i = 0; i < car_count; i++) {
         if (cars[i].arrival < earliest_arrival) {
             earliest_arrival = cars[i].arrival;
-            strcpy(current_direction, cars[i].direction);
-            strcpy(next_direction, cars[i].direction);
+            current_direction = cars[i].direction;
+            next_direction = cars[i].direction;
         }
     }
 
@@ -87,19 +86,17 @@ int main(int argc, char **argv) {
     }
 
     // Print initial direction
-    if (strcmp(current_direction, "N") == 0) {
+    if (current_direction == 'N') {
         printf("Direction: North\n");
     } else {
         printf("Direction: South\n");
     }
 
-    // Create and all threads
+    // Create all threads
     pthread_t threads[car_count];
     for (i = 0; i < car_count; i++) {
         pthread_create(&threads[i], NULL, &car_thread_execute, &cars[i]);
     }
-
-    // printf("\nCurrent time: %d, current direction: %s, next direction: %s\n", current_time, current_direction, next_direction);
 
     int is_done = 0;
     while (is_done == 0) {
@@ -110,23 +107,10 @@ int main(int argc, char **argv) {
                 is_done = 0;
                 break;
             }
-            if (i == car_count-1) {
-                // printf("--- All cars have exited the bridge ---\n");
-            }
         }
 
         sem_wait(&time_sem);
         current_time++;
-
-        // Avoid STARVATION: Allow cars in other direction to advance
-        // if (cars_since_direction_change == 5) {
-        //         if (strcmp(next_direction, "N") == 0) {
-        //             strcpy(next_direction, "S");
-        //         } else {
-        //             strcpy(next_direction, "N");
-        //         }
-        //         cars_since_direction_change = 0;
-        // }
 
         // Reset all car threads
         for (i = 0; i < car_count; i++) {
@@ -139,23 +123,15 @@ int main(int argc, char **argv) {
         }
     }
 
-    printf("Main got here 1\n");
-
-    // Join all threads
-    // for (i = 0; i < car_count; i++) {
-    //     pthread_join(threads[i], NULL);
-    // }
-
-    printf("Main got here 2\n");
-
-    // Destroy all semaphores
-    // sem_destroy(&time_sem);
-    // sem_destroy(&bridge_sem);
-    // for (i = 0; i < car_count; i++) {
-    //     sem_destroy(&car_sem[i]);
-    // }
+    // All threads complete, destroy all semaphores
+    sem_destroy(&time_sem);
+    sem_destroy(&bridge_sem);
+    for (i = 0; i < car_count; i++) {
+        sem_destroy(&car_sem[i]);
+    }
 }
 
+// Function to be executed by car threads
 void *car_thread_execute(void *car_ptr) {
     int i;
     Car *car = (Car *)car_ptr;
@@ -166,16 +142,14 @@ void *car_thread_execute(void *car_ptr) {
         thread_num++;
     sem_post(&bridge_sem);
 
-    int done = 0;
-    while (done == 0) {
+    int is_done = 0;
+    while (is_done == 0) {
         // Wait if car has not entered the bridge
         while ((current_time < car->arrival) ||
-               (strcmp(current_direction, car->direction) != 0 && cars_on_bridge != 0)) {
-                // printf("%s still waiting\n", car->car);
-                // printf("time: %d, arrival: %d\n", current_time, car->arrival);
-                // printf("current direction: %s, cars on bridge: %d\n", current_direction, cars_on_bridge);
-                // printf("next direction: %s, duration: %d, duration left: %d\n", current_direction, car->duration, car->duration_left);
+               (current_direction != car->direction && cars_on_bridge != 0) ||
+               (current_direction == car->direction && cars_since_direction_change >= 5 && car->duration == car->duration_left && cars_on_bridge != 0)) {
 
+            // Check if all threads are done
             sem_wait(&bridge_sem);
                 car->done = 1;
                 int alldone = 1;
@@ -188,34 +162,35 @@ void *car_thread_execute(void *car_ptr) {
                     sem_post(&time_sem);
                 }
             sem_post(&bridge_sem);
-            // Wait
+
+            // Wait for time increment
             sem_wait(thread_sem);
         }
 
-        // Car enters the bridge
         sem_wait(&bridge_sem);
             // Change the current direction
-            if (strcmp(current_direction, car->direction) != 0) {
-                strcpy(current_direction, car->direction);
-                if (strcmp(current_direction, "N") == 0) {
+            if (current_direction != car->direction) {
+                current_direction =  car->direction;
+                cars_since_direction_change = 0;
+                if (current_direction == 'N') {
                     printf("Direction: North\n");
                 } else {
                     printf("Direction: South\n");
                 }
             }
+
+            // Car enters the bridge
             if (car->duration == car->duration_left) {
                 cars_on_bridge++;
                 cars_since_direction_change++;
                 printf("%s\n", car->car);
             }
-        sem_post(&bridge_sem);
 
-        // Advance on bridge
-        car->duration_left--;
-        car->done = 1;
+            // Advance on bridge
+            car->duration_left--;
+            car->done = 1;
 
-        // Check if all threads are done
-        sem_wait(&bridge_sem);
+            // Check if all threads are done
             int alldone = 1;
             for (i = 0; i < car_count; i++) {
                 if (cars[i].done != 1) {
@@ -227,16 +202,16 @@ void *car_thread_execute(void *car_ptr) {
             }
         sem_post(&bridge_sem);
 
-        // Wait
+        // Wait for time increment
         sem_wait(thread_sem);
 
         // Car exits the bridge
         sem_wait(&bridge_sem);
             if (car->duration_left <= 0) {
                 cars_on_bridge--;
-                car->exited = 1;
                 car->done = 1;
-                // printf("%s exits the bridge, number of cars on bridge: %d\n", car->car, cars_on_bridge);
+                car->exited = 1;
+
                 // Check if all threads are done
                 int alldone = 1;
                 for (i = 0; i < car_count; i++) {
@@ -247,7 +222,7 @@ void *car_thread_execute(void *car_ptr) {
                 if (alldone == 1) {
                     sem_post(&time_sem);
                 }
-                done = 1;
+                is_done = 1;
             }
         sem_post(&bridge_sem);
     }
@@ -291,7 +266,7 @@ int getProcesses(Car **cars, size_t *cars_size) {
             if (token_num == 0) {
                 (*cars)[cars_count].car = strdup(token);
             } else if (token_num == 1) {
-                (*cars)[cars_count].direction = strdup(token);
+                (*cars)[cars_count].direction = token[0];
             } else if (token_num == 2) {
                 (*cars)[cars_count].arrival = atoi(token);
             } else if (token_num == 3) {
